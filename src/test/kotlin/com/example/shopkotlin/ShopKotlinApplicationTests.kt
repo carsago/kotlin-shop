3f3aa7e73ce4b83ef7b122002937eefbf2f319a7
hyunseo <>
2023-11-18 01:08:55 +0900

    init
    (cherry picked from commit fb567755e18bddc83ae456e579efb08e4e343dad)

---
+++ src/test/kotlin/com/example/shopkotlin/ShopKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.shopkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShopKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
